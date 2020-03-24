ff6a4c40948477f88f19e2c9d1d60cd8c7378902
RFPadilha <34008458+RFPadilha@users.noreply.github.com>
2020-03-24 17:51:00 -0300

    3D Smash update

---
+++ New Unity Project/Assets/Scripts/WeaponColliderScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class WeaponColliderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(gameObject.name + "was triggered by" + other.gameObject.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log(other.gameObject.name + "takes damage");
+        }
+    }
+}
