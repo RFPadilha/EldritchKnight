4e73e05afd63551304bebb98b845285f17d14e5e
RFPadilha <34008458+RFPadilha@users.noreply.github.com>
2020-05-04 17:07:59 -0300

    Implemented basic enemy
    -combat system will follow after

--- New Unity Project/Assets/Scripts/EnemyController.cs
+++ New Unity Project/Assets/Scripts/EnemyController.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float lookRadius = 10f;
+    public Transform target;
+    Vector3 startingPosition;
+    NavMeshAgent agent;
     void Start()
     {
-        
+        startingPosition = new Vector3(12.8f, 2.2f, 6.7f);
+        agent = GetComponent<NavMeshAgent>();
+        agent.Warp(startingPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
